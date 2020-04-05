72b441231ce0556d055254f50ce25adb8f53ea50
ggaoshen <shen.gao@carta.com>
2020-04-05 02:38:18 -0700

    create first model and migration. update admin setting

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from imagekit.models import ProcessedImageField # need to add imagekit to app directory, otw can't import
 
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField(blank=True, null=True) # title can be blank or null
+    image = ProcessedImageField(
+        upload_to = 'static/images/posts',
+        format = 'JPEG', 
+        options = {'quality': 100}, 
+        blank = True, 
+        null = True
+    )
+
+class PostTwo(models.Model):
+    title = models.TextField(blank=True, null=True) # title can be blank or null
+    image = ProcessedImageField(
+        upload_to = 'static/images/posts',
+        format = 'JPEG', 
+        options = {'quality': 100}, 
+        blank = True, 
+        null = True
+    )
