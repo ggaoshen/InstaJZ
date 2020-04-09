07ffd883d0905089b553f2b7546eaf739d129972
ggaoshen <shen.gao@carta.com>
2020-04-09 02:49:54 -0700

    add static files storage for whitenoise

--- InstaJZ/settings.py
+++ InstaJZ/settings.py
@@ -121,11 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# need for whitenoise to upload local static files to temp cloud, which tricks heroku into thinking we have static stored in cloud
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
